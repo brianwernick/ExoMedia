9d121c4d2e1c40b2adaee3d08924042bd77612ed
Brian Wernick <BrianWernick@gmail.com>
2015-07-24 09:52:17 -0600

    Fixed inconsistencies in the event variable availability

--- library/src/main/java/com/devbrackets/android/exomedia/event/EMMediaAllowedTypeChangedEvent.java
+++ library/src/main/java/com/devbrackets/android/exomedia/event/EMMediaAllowedTypeChangedEvent.java
@@ -19,9 +19,13 @@ package com.devbrackets.android.exomedia.event;
 import com.devbrackets.android.exomedia.manager.EMPlaylistManager;
 
 public class EMMediaAllowedTypeChangedEvent {
-    public final EMPlaylistManager.MediaType allowedType;
+    private final EMPlaylistManager.MediaType allowedType;
 
     public EMMediaAllowedTypeChangedEvent(EMPlaylistManager.MediaType allowedType) {
         this.allowedType = allowedType;
+    }
+
+    public EMPlaylistManager.MediaType getAllowedType() {
+        return allowedType;
     }
 }

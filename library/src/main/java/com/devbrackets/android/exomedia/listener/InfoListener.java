359bc60642051655abf3f584790041cbddb630ac
Brian Wernick <BrianWernick@gmail.com>
2015-09-29 18:58:20 -0600

    Updated README Converted to work with the ExoPlayer 1.5.0 release

--- library/src/main/java/com/devbrackets/android/exomedia/listener/InfoListener.java
+++ library/src/main/java/com/devbrackets/android/exomedia/listener/InfoListener.java
@@ -24,19 +24,19 @@ import com.google.android.exoplayer.chunk.Format;
  * A listener for debugging EMExoPlayer information.
  */
 public interface InfoListener {
-    void onVideoFormatEnabled(Format format, int trigger, int mediaTimeMs);
+    void onVideoFormatEnabled(Format format, int trigger, long mediaTimeMs);
 
-    void onAudioFormatEnabled(Format format, int trigger, int mediaTimeMs);
+    void onAudioFormatEnabled(Format format, int trigger, long mediaTimeMs);
 
     void onDroppedFrames(int count, long elapsed);
 
     void onBandwidthSample(int elapsedMs, long bytes, long bitrateEstimate);
 
-    void onLoadStarted(int sourceId, long length, int type, int trigger, Format format, int mediaStartTimeMs, int mediaEndTimeMs);
+    void onLoadStarted(int sourceId, long length, int type, int trigger, Format format, long mediaStartTimeMs, long mediaEndTimeMs);
 
-    void onLoadCompleted(int sourceId, long bytesLoaded, int type, int trigger, Format format, int mediaStartTimeMs, int mediaEndTimeMs, long elapsedRealtimeMs, long loadDurationMs);
+    void onLoadCompleted(int sourceId, long bytesLoaded, int type, int trigger, Format format, long mediaStartTimeMs, long mediaEndTimeMs, long elapsedRealtimeMs, long loadDurationMs);
 
     void onDecoderInitialized(String decoderName, long elapsedRealtimeMs, long initializationDurationMs);
 
-    void onSeekRangeChanged(TimeRange seekRange);
+    void onAvailableRangeChanged(TimeRange availableRange);
 }

faefcac948535d2ba970c8141c6c75eecd3ca44d
Brian Wernick <BrianWernick@Gmail.com>
2016-07-06 14:43:20 -0600

    Removed the temporary `penaltyDeath()` in the demo app

--- demo/src/main/java/com/devbrackets/android/exomediademo/App.java
+++ demo/src/main/java/com/devbrackets/android/exomediademo/App.java
@@ -47,7 +47,6 @@ public class App extends Application {
                 .detectLeakedSqlLiteObjects()
                 .detectLeakedClosableObjects()
                 .penaltyLog()
-                .penaltyDeath()
                 .build());
     }
 }

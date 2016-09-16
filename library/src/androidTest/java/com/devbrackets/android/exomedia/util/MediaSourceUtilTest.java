29b48fefcbdc32f6b45685a942ae2fdaf47f74ff
Karol Kowalski <ordon1313@gmail.com>
2016-09-16 12:12:34 +0200

    Add JUnit4 support for android tests

---
+++ library/src/androidTest/java/com/devbrackets/android/exomedia/util/MediaSourceUtilTest.java
@@ -0,0 +1,13 @@
+package com.devbrackets.android.exomedia.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MediaSourceUtilTest {
+
+    @Test
+    public void exampleTest() {
+        Assert.assertTrue(true);
+    }
+
+}

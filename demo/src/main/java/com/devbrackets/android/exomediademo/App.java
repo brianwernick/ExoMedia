dff7b531bb3d55480e5d2f8e893910dbfe833475
Brian Wernick <brianwernick@users.noreply.github.com>
2017-08-14 08:40:04 -0600

    Deprecated the HttpDataSourceFactoryProvider in favor of the more permissive and aptly named DataSourceFactoryProvider (#504)

--- demo/src/main/java/com/devbrackets/android/exomediademo/App.java
+++ demo/src/main/java/com/devbrackets/android/exomediademo/App.java
@@ -9,8 +9,12 @@ import com.devbrackets.android.exomedia.ExoMedia;
 import com.devbrackets.android.exomediademo.manager.PlaylistManager;
 import com.google.android.exoplayer2.ext.okhttp.OkHttpDataSourceFactory;
 import com.google.android.exoplayer2.upstream.DataSource;
-import com.google.android.exoplayer2.upstream.HttpDataSource;
 import com.google.android.exoplayer2.upstream.TransferListener;
+import com.google.android.exoplayer2.upstream.cache.Cache;
+import com.google.android.exoplayer2.upstream.cache.CacheDataSource;
+import com.google.android.exoplayer2.upstream.cache.CacheDataSourceFactory;
+import com.google.android.exoplayer2.upstream.cache.LeastRecentlyUsedCacheEvictor;
+import com.google.android.exoplayer2.upstream.cache.SimpleCache;
 import com.squareup.leakcanary.LeakCanary;
 
 import okhttp3.OkHttpClient;
@@ -66,11 +70,16 @@ public class App extends Application {
     private void configureExoMedia() {
         // Registers the media sources to use the OkHttp client instead of the standard Apache one
         // Note: the OkHttpDataSourceFactory can be found in the ExoPlayer extension library `extension-okhttp`
-        ExoMedia.setHttpDataSourceFactoryProvider(new ExoMedia.HttpDataSourceFactoryProvider() {
+        ExoMedia.setDataSourceFactoryProvider(new ExoMedia.DataSourceFactoryProvider() {
             @NonNull
             @Override
-            public HttpDataSource.BaseFactory provide(@NonNull String userAgent, @Nullable TransferListener<? super DataSource> listener) {
-                return new OkHttpDataSourceFactory(new OkHttpClient(), userAgent, listener);
+            public DataSource.Factory provide(@NonNull String userAgent, @Nullable TransferListener<? super DataSource> listener) {
+                // Updates the network data source to use the OKHttp implementation
+                DataSource.Factory upstreamFactory = new OkHttpDataSourceFactory(new OkHttpClient(), userAgent, listener);
+
+                // Adds a cache around the upstreamFactory
+                Cache cache = new SimpleCache(getCacheDir(), new LeastRecentlyUsedCacheEvictor(50 * 1024 * 1024));
+                return new CacheDataSourceFactory(cache, upstreamFactory, CacheDataSource.FLAG_IGNORE_CACHE_ON_ERROR);
             }
         });
     }

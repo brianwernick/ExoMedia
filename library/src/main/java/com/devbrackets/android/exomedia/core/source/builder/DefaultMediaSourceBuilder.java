8636fc2c671ddd67684b86167ef1d5e97e76569a
Jonathan Trowbridge <jonathan.trowbridge@gmail.com>
2017-11-22 07:57:48 -0700

    subtitles - Created SubtitleUtil class..

--- library/src/main/java/com/devbrackets/android/exomedia/core/source/builder/DefaultMediaSourceBuilder.java
+++ library/src/main/java/com/devbrackets/android/exomedia/core/source/builder/DefaultMediaSourceBuilder.java
@@ -6,16 +6,13 @@ import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
-import com.google.android.exoplayer2.C;
-import com.google.android.exoplayer2.Format;
+import com.devbrackets.android.exomedia.util.SubtitleUtil;
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.source.MergingMediaSource;
-import com.google.android.exoplayer2.source.SingleSampleMediaSource;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.TransferListener;
-import com.google.android.exoplayer2.util.MimeTypes;
 
 public class DefaultMediaSourceBuilder extends MediaSourceBuilder {
     @NonNull
@@ -23,17 +20,24 @@ public class DefaultMediaSourceBuilder extends MediaSourceBuilder {
     public MediaSource build(@NonNull Context context, @NonNull Uri uri, @NonNull String userAgent, @NonNull Handler handler, @Nullable TransferListener<? super DataSource> transferListener) {
         DataSource.Factory dataSourceFactory = buildDataSourceFactory(context, userAgent, transferListener);
 
-        Format textFormat = Format.createTextSampleFormat(null, MimeTypes.TEXT_VTT, null, Format.NO_VALUE, Format.NO_VALUE, "en", null, 0);
-        Uri srtUri = Uri.parse("http://www.drakonesia.com/18.vtt");
+        Uri captionUri = Uri.parse("http://www.maxovahra.com/TAP_MAXSGAME_SUBS_GR.srt");
 
-        MediaSource textMediaSource = new SingleSampleMediaSource(srtUri, dataSourceFactory,
-                textFormat, C.TIME_UNSET);
+        ExtractorMediaSource mediaSource = new ExtractorMediaSource(
+                uri,
+                dataSourceFactory,
+                new DefaultExtractorsFactory(),
+                handler,
+                null
+        );
 
-        ExtractorMediaSource mediaSource = new ExtractorMediaSource(uri, dataSourceFactory, new DefaultExtractorsFactory(), handler, null);
-
-        return new MergingMediaSource(
-                textMediaSource,
-                mediaSource
+        return captionUri == null
+                ? mediaSource
+                : SubtitleUtil.createMergingMediaSource(
+                dataSourceFactory,
+                mediaSource,
+                captionUri
         );
     }
+
+
 }
---
+++ library/src/main/java/com/devbrackets/android/exomedia/util/SubtitleUtil.java
@@ -0,0 +1,104 @@
+package com.devbrackets.android.exomedia.util;
+
+
+import android.net.Uri;
+import android.webkit.MimeTypeMap;
+
+import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.Format;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.MergingMediaSource;
+import com.google.android.exoplayer2.source.SingleSampleMediaSource;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.util.MimeTypes;
+
+
+/**
+ * This utility class provides static utility methods for subtitles.
+ */
+public class SubtitleUtil {
+
+    private void SubtitleView() {
+    }
+
+    /**
+     * Get the subtitle MimeTypes value from the provided Uri. If the provided
+     * Uri is not recognized as either SRT or VTT, then null is returned.
+     *
+     * @param uri The subtitle Uri.
+     * @return If recognized, the associated MimeTypes value. Else, null.
+     */
+    public static String getSubtitleMimeType(Uri uri) {
+        String extension = MimeTypeMap
+                .getFileExtensionFromUrl(uri.toString())
+                .toLowerCase();
+
+        switch (extension) {
+            case "srt":
+                return MimeTypes.APPLICATION_SUBRIP;
+            case "vtt":
+                return MimeTypes.TEXT_VTT;
+            default:
+                return null;
+        }
+    }
+
+
+    /**
+     * Create a subtitle MediaSource instance.
+     *
+     * @param dataSourceFactory The data source factory.
+     * @param uri               The Uri of the subtitles.
+     * @return A new SingleSampleMediaSource instance for the provided
+     * subtitle Uri.
+     */
+    public static MediaSource createSubtitleMediaSource(
+            DataSource.Factory dataSourceFactory,
+            Uri uri
+    ) {
+        return new SingleSampleMediaSource(
+                uri,
+                dataSourceFactory,
+                Format.createTextSampleFormat(
+                        null,
+                        getSubtitleMimeType(uri),
+                        null,
+                        Format.NO_VALUE,
+                        Format.NO_VALUE,
+                        "en",
+                        null,
+                        0
+                ),
+                C.TIME_UNSET
+        );
+    }
+
+    /**
+     * Create a new MediaSource instance that combines the provided MediaSource
+     * and MediaSource created from the provided caption Uri.
+     *
+     * @param dataSourceFactory The data source factory.
+     * @param mediaSource       The media source.
+     * @param captionUri        The related caption Uri.
+     * @return The resultant MediaSource instance from combining the two sources.
+     */
+    public static MediaSource createMergingMediaSource(
+            DataSource.Factory dataSourceFactory,
+            MediaSource mediaSource,
+            Uri captionUri
+    ) {
+        if (captionUri == null) {
+            return mediaSource;
+        }
+
+        return new MergingMediaSource(
+                createSubtitleMediaSource(
+                        dataSourceFactory,
+                        captionUri
+                ),
+                mediaSource
+        );
+    }
+
+
+}

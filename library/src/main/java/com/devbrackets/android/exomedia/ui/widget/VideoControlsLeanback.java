c3e27a71af7b9e90d7d0ea803a6a0bbee35e89b3
Brian Wernick <brianwernick@users.noreply.github.com>
2018-05-08 11:59:38 -0600

    Updated the leanback styles (#613)

--- library/src/main/java/com/devbrackets/android/exomedia/ui/widget/VideoControlsLeanback.java
+++ library/src/main/java/com/devbrackets/android/exomedia/ui/widget/VideoControlsLeanback.java
@@ -193,12 +193,17 @@ public class VideoControlsLeanback extends VideoControls {
 
     @Override
     protected void updateButtonDrawables() {
-        super.updateButtonDrawables();
+        updateButtonDrawables(R.color.exomedia_default_controls_leanback_button_selector);
+    }
 
-        Drawable rewindDrawable = ResourceUtil.tintList(getContext(), R.drawable.exomedia_ic_rewind_white, R.color.exomedia_default_controls_button_selector);
+    @Override
+    protected void updateButtonDrawables(int tintList) {
+        super.updateButtonDrawables(tintList);
+
+        Drawable rewindDrawable = ResourceUtil.tintList(getContext(), R.drawable.exomedia_ic_rewind_white, tintList);
         rewindButton.setImageDrawable(rewindDrawable);
 
-        Drawable fastForwardDrawable = ResourceUtil.tintList(getContext(), R.drawable.exomedia_ic_fast_forward_white, R.color.exomedia_default_controls_button_selector);
+        Drawable fastForwardDrawable = ResourceUtil.tintList(getContext(), R.drawable.exomedia_ic_fast_forward_white, tintList);
         fastForwardButton.setImageDrawable(fastForwardDrawable);
     }
 
